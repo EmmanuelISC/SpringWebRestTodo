cf1caaa1dba56a710c37c2bf69aa17b164de0ee3
Emmanuel-psy <emmanuel_elias_94@hotmail.com>
2021-09-14 11:56:41 -0500

    Stable version enabling basic spring secure part

--- src/main/java/com/eeliasISC/restfulwebservices/basic/auth/SpringSecurityConfigurationAuth.java
+++ src/main/java/com/eeliasISC/restfulwebservices/basic/auth/SpringSecurityConfigurationAuth.java
@@ -2,31 +2,29 @@ package com.eeliasISC.restfulwebservices.basic.auth;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@Configuration
-//@EnableWebSecurity
-public class SpringSecurityConfigurationAuth /*extends WebSecurityConfigurerAdapter*/{
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfigurationAuth extends WebSecurityConfigurerAdapter{
 	
-	/* @Override
+	 @Override
 	    protected void configure(HttpSecurity http) throws Exception {
 	 
-	 
 	                http
-	                        .csrf().disable()
-	                        .authorizeRequests()
-	                        .antMatchers(HttpMethod.GET, "/**").permitAll()
-	                        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-	                        .antMatchers(HttpMethod.PUT, "/**").permitAll()
-	                        .antMatchers(HttpMethod.DELETE, "/**").permitAll()
-	                        .anyRequest()
-	                .authenticated()
-	                .and()
-	               // .formLogin()
-	               // .and()
-	                .httpBasic();
-	    }*/
+	                .csrf().disable()
+	                    .authorizeRequests()
+	                    .antMatchers(HttpMethod.GET, "/**").permitAll()
+	                    .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+	                    .antMatchers(HttpMethod.POST, "/**").permitAll()
+	                    .antMatchers(HttpMethod.PUT, "/**").permitAll()
+	                    .antMatchers(HttpMethod.DELETE, "/**").permitAll()
+	                    .anyRequest().authenticated()
+	                    .and()
+	                    //.formLogin().and()
+	                    .httpBasic();
+	    }
 
 }
